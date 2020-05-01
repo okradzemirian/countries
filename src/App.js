8aefa5572d49ebc17cb393a042a38b536fcb0cc3
Mirian Okradze <okradzemirian@gmail.com>
2020-05-01 17:35:50 +0400

    render countries data

--- src/App.js
+++ src/App.js
@@ -9,9 +9,22 @@ function App() {
         axios.get('https://restcountries.eu/rest/v2/all').then(res => {
             setCountries(res.data)
         })
-    })
+    }, [])
 
-    return <div className='App'></div>
+    return (
+        <div className='App'>
+            {countries &&
+                countries.map(({ flag, name, region, population, capital }) => (
+                    <div>
+                        <img src={flag} alt={`${name} flag`} />
+                        <h2>{name}</h2>
+                        <p>Population: {population}</p>
+                        <p>Region: {region}</p>
+                        <p>Capital: {capital}</p>
+                    </div>
+                ))}
+        </div>
+    )
 }
 
 export default App
