1fe7329984447eb56d9e739220bb69b12a9b8198
Mirian Okradze <okradzemirian@gmail.com>
2020-05-01 17:06:23 +0400

    get countries data on load and set to state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.scss'
 
 function App() {
+    const [countries, setCountries] = useState(null)
+
+    useEffect(() => {
+        axios.get('https://restcountries.eu/rest/v2/all').then(res => {
+            setCountries(res.data)
+        })
+    })
+
     return <div className='App'></div>
 }
 
