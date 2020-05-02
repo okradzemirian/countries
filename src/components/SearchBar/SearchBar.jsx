2e2c10a9849e6adce626e0e3f7a12174d9921fad
Mirian Okradze <okradzemirian@gmail.com>
2020-05-02 15:55:53 +0400

    added state to SearchBar input

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SearchBar.scss'
 
 const SearchBar = () => {
+    const [searchText, setSearchText] = useState('')
+
+    const handleChange = e => {
+        setSearchText(e.target.value)
+    }
+
     return (
         <div className='search-bar'>
             <input
+                onChange={handleChange}
+                value={searchText}
                 className='search-bar__input'
                 type='search'
                 placeholder='Search for a country...'
