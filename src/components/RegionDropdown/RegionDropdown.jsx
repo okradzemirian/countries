88f3154da07dbc6e0ad5d93f5470adc02f849c7c
Mirian Okradze <okradzemirian@gmail.com>
2020-05-02 14:15:11 +0400

    created RegionDropdown component and styled

---
+++ src/components/RegionDropdown/RegionDropdown.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './RegionDropdown.scss'
+
+const RegionDropdown = () => {
+    return (
+        <select className='dropdown'>
+            <option value='africa'>Africa</option>
+            <option value='america'>America</option>
+            <option value='asia'>Asia</option>
+            <option value='europe'>Europe</option>
+            <option value='oceania'>Oceania</option>
+        </select>
+    )
+}
+
+export default RegionDropdown
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import SearchBar from '../../components/SearchBar/SearchBar'
+import RegionDropdown from '../../components/RegionDropdown/RegionDropdown'
 import CountryList from '../../components/CountryList/CountryList'
 import './Homepage.scss'
 
@@ -14,8 +15,9 @@ const Homepage = () => {
     }, [])
 
     return (
-        <div className='wrapper'>
+        <div className='wrapper homepage'>
             <SearchBar />
+            <RegionDropdown />
             <CountryList countries={countries} />
         </div>
     )
