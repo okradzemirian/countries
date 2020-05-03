249ef1d5195abb2afc8cc15a2f93441e71fba9f4
Mirian Okradze <okradzemirian@gmail.com>
2020-05-03 21:40:45 +0400

    created filter types and added reducer cases and actions

--- src/redux/filters/filtersReducer.js
+++ src/redux/filters/filtersReducer.js
@@ -1,3 +1,5 @@
+import { SET_REGION, SET_SEARCH_TEXT } from './filtersTypes'
+
 const initialState = {
     searchText: '',
     region: '',
@@ -5,6 +7,16 @@ const initialState = {
 
 const filtersReducer = (state = initialState, action) => {
     switch (action.type) {
+        case SET_REGION:
+            return {
+                ...state,
+                region: action.payload,
+            }
+        case SET_SEARCH_TEXT:
+            return {
+                ...state,
+                searchText: action.payload,
+            }
         default:
             return state
     }
