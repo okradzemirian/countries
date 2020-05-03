67517ceb5a75d1b209e5ccb110e0f248bcf1a125
Mirian Okradze <okradzemirian@gmail.com>
2020-05-03 21:33:48 +0400

    created filters reducer boilerplate

---
+++ src/redux/filters/filtersReducer.js
@@ -0,0 +1,13 @@
+const initialState = {
+    searchText: '',
+    region: '',
+}
+
+const filtersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state
+    }
+}
+
+export default filtersReducer
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux'
+import filtersReducer from './filters/filtersReducer'
 
-const rootReducer = combineReducers({})
+const rootReducer = combineReducers({
+    filters: filtersReducer,
+})
 
 export default rootReducer
