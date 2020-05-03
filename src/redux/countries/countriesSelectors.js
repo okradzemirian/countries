11e8ecffbf2e1f26f97e3aee5aedea13f14847fc
Mirian Okradze <okradzemirian@gmail.com>
2020-05-03 23:46:24 +0400

    added search filter functionality on countries

--- src/redux/countries/countriesSelectors.js
+++ src/redux/countries/countriesSelectors.js
@@ -1,8 +1,18 @@
 import { createSelector } from 'reselect'
 
 const countries = state => state.countries
+const filters = state => state.filters
 
 export const selectCountries = createSelector(
-    [countries],
-    countries => countries.countries,
+    [countries, filters],
+    (countries, filters) => {
+        if (countries.countries) {
+            return countries.countries.filter(country =>
+                country.name
+                    .toLowerCase()
+                    .includes(filters.searchText.toLowerCase()),
+            )
+        }
+        return null
+    },
 )
