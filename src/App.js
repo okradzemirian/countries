127789f3c9dabb5dc75047412382823c88047228
Mirian Okradze <okradzemirian@gmail.com>
2020-05-04 16:13:43 +0400

    initialized react-router-dom and created main route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
 import './App.scss'
 
 function App() {
     return (
-        <div className='App'>
-            <Homepage />
-        </div>
+        <BrowserRouter>
+            <div className='App'>
+                <Switch>
+                    <Route exact path='/' component={Homepage} />
+                </Switch>
+            </div>
+        </BrowserRouter>
     )
 }
 
