a30a1ae3c4df176acdc5d0502b68a10474c06f41
Mirian Okradze <okradzemirian@gmail.com>
2020-05-04 16:27:32 +0400

    created route for Countrypage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import Homepage from './pages/Homepage/Homepage'
+import Countrypage from './pages/Countrypage/Countrypage'
 import './App.scss'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
             <div className='App'>
                 <Switch>
                     <Route exact path='/' component={Homepage} />
+                    <Route path='/:countryName' component={Countrypage} />
                 </Switch>
             </div>
         </BrowserRouter>
---
+++ src/pages/Countrypage/Countrypage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Countrypage.scss'
+
+const Countrypage = () => {
+    return <div>Detailed Country Page</div>
+}
+
+export default Countrypage
