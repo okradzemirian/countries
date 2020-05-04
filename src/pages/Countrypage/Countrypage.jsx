79bb5761d110f31e7c953c40fc9921d144d54741
Mirian Okradze <okradzemirian@gmail.com>
2020-05-04 16:37:17 +0400

    added fetching logic in Countrypage

--- src/pages/Countrypage/Countrypage.jsx
+++ src/pages/Countrypage/Countrypage.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './Countrypage.scss'
 
-const Countrypage = () => {
+const Countrypage = ({ match }) => {
+    const [country, setCountry] = useState(null)
+
+    const { countryName } = match.params
+
+    useEffect(() => {
+        axios
+            .get(
+                `https://restcountries.eu/rest/v2/name/${countryName}/?fullText=true`,
+            )
+            .then(res => {
+                setCountry(res.data[0])
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
+        return () => {
+            setCountry(null)
+        }
+    }, [countryName])
+
     return <div>Detailed Country Page</div>
 }
 
