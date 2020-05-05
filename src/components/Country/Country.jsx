9d4c13f37d9e49980b6db51a4ecfa9686c7947ce
Mirian Okradze <okradzemirian@gmail.com>
2020-05-05 19:25:55 +0400

    styled Country component

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -16,54 +16,75 @@ const Country = ({ country }) => {
         borders,
     } = country
 
-    console.log(country)
-
     return (
         <div className='country'>
-            <div className='country__image-wrapper'>
-                <img src={flag} alt={`${name} flag`} />
+            <div className='country__flag-wrapper'>
+                <img
+                    className='country__flag'
+                    src={flag}
+                    alt={`${name} flag`}
+                />
             </div>
-            <div>
-                <h2>{name}</h2>
-                <ul>
-                    <li>Native Name: {nativeName}</li>
-                    <li>Population: {population}</li>
-                    <li>Region: {region}</li>
-                    <li>Sub Region: {subregion}</li>
-                    <li>Capital: {capital}</li>
-                    <li>
-                        Top Level Domain:
-                        <ul>
-                            {topLevelDomain.map(domain => (
-                                <li key={domain}>{domain}</li>
-                            ))}
-                        </ul>
-                    </li>
-                    <li>
-                        Currency:
-                        <ul>
-                            {currencies.map(({ code }) => (
-                                <li key={code}>{code}</li>
-                            ))}
-                        </ul>
-                    </li>
-                    <li>
-                        Languages:
-                        <ul>
-                            {languages.map(({ name }) => (
-                                <li key={name}>{name}</li>
-                            ))}
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <h4>Borders:</h4>
-                <ul>
-                    {borders.map(country => (
-                        <li key={country}>{country}</li>
-                    ))}
-                </ul>
+            <div className='country__content'>
+                <h2 className='country__name'>{name}</h2>
+                <div className='country__info-wrapper'>
+                    <ul className='country__info-list'>
+                        <li className='country__info'>
+                            <span>Native Name:</span> {nativeName}
+                        </li>
+                        <li className='country__info'>
+                            <span>Population: </span>
+                            {population}
+                        </li>
+                        <li className='country__info'>
+                            <span>Region: </span>
+                            {region}
+                        </li>
+                        <li className='country__info'>
+                            <span>Sub Region:</span> {subregion}
+                        </li>
+                        <li className='country__info'>
+                            <span>Capital: </span>
+                            {capital}
+                        </li>
+                    </ul>
+                    <ul>
+                        <li className='country__info'>
+                            <span>Top Level Domain:</span>
+                            <ul>
+                                {topLevelDomain.map(domain => (
+                                    <li key={domain}>{domain}</li>
+                                ))}
+                            </ul>
+                        </li>
+                        <li className='country__info'>
+                            <span>Currency:</span>
+                            <ul>
+                                {currencies.map(({ code }) => (
+                                    <li key={code}>{code}</li>
+                                ))}
+                            </ul>
+                        </li>
+                        <li className='country__info'>
+                            <span>Languages:</span>
+                            <ul>
+                                {languages.map(({ name }) => (
+                                    <li key={name}>{name}</li>
+                                ))}
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+                <div className='borders'>
+                    <h4 className='borders__title'>Borders:</h4>
+                    <ul className='borders__list'>
+                        {borders.map(country => (
+                            <li key={country} className='borders__item'>
+                                {country}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </div>
         </div>
     )
