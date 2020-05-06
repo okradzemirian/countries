f878af3c665a29bc0f11c2914caa2cef684f7bbe
Mirian Okradze <okradzemirian@gmail.com>
2020-05-06 16:43:15 +0400

    added Link in header to homepage and fixed content moving when scrollbar hides

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './Header.scss'
 
 const Header = () => {
     return (
         <header className='header'>
             <div className='wrapper header__wrapper'>
-                <h2 className='header__title'>Where In The World?</h2>
+                <h2 className='header__title'>
+                    <Link className='header__link' to='/'>
+                        Where In The World?
+                    </Link>
+                </h2>
                 <button className='header__button'>Dark Mode</button>
             </div>
         </header>
